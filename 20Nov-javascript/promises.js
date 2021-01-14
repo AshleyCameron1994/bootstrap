611ac3985c137819883bc6d25d7732e1b1b035e3
Ashley-Cameron <AshleyCameron1994@outlook.com>
2021-01-14 10:48:57 +0000

    promises added

---
+++ 20Nov-javascript/promises.js
@@ -0,0 +1,72 @@
+'use strict'; 
+
+// // callbacks - a function that takes in a function
+
+// // first function
+// const greeting = (fullname) => {
+//     alert(`Hello ${fullname}`);
+// }
+
+// // second function 
+// const processUserInput = (callback) => {
+//     // get the user to type their name 
+//     let humansname = prompt("Please enter your name"); 
+//     // once the user has entered their name 
+//     callback(humansname);
+// }
+
+// processUserInput(greeting);
+
+
+// // resvole = .then();
+// // reject = .catch();
+
+// let promprom = new Promise((resolve,reject) => {
+//     let a = 10+10; 
+//     if(a == 297834923849){
+//         // successful outcome
+//         resolve("All went well!");
+//     }else{
+//         // something went wrong
+//         reject("It didn't work!");
+//     }
+// }); 
+
+// promprom.then((message)=>{
+//     console.log(`This is in the then block, the status of the promise is ${message}`);
+// }).catch((message) => {
+//     console.log(`This is in the catch block, status is ${message}`);
+// }).then(() => {
+//     console.log(`I will take place regardless of the what happened before!`);
+// });
+
+// let aNewPromise = new Promise((resolve,reject)=>{
+//     console.log(`initial`);
+//     resolve();
+//     reject(); // unreachablecode
+// }).then(() => {
+//     // throw new Error("Something has failed"); // Throws error
+//     console.log("Do this"); // unreachable code
+// }).then(()=>{
+//     console.log(`Do this after whatever happened before`);
+// }).catch((message) => {
+//     console.log(`Catches ${message}`);
+// });
+
+function runPromise(){
+    let aPromise = new Promise((resolve, reject) => {
+        let delayedFunc = setTimeout(() => {
+            
+        let randomNumber = Math.random();
+
+        (randomNumber<0.5) ? resolve(randomNumber) : reject(randomNumber);
+        }, Math.random() * 5000);
+    })
+    
+        aPromise.then((data) =>{
+            console.log(`This was below 0.5 ${data}`);
+        }).catch((error) => {
+            console.log(`This was above 0.5: ${error}`);
+        })
+
+runPromise()};
